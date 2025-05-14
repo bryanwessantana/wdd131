f7b28d350e2c2670adcb87abb687210b382c5305
bryanwessantana <bryanwessantana@gmail.com>
2025-05-13 21:03:19 -0300

    W01 Assignment: Home Page

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,3 +1,2 @@
-document.getElementById("currentYear").textContent = new Date().getFullYear();
-
+document.getElementById("currentyear").textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
